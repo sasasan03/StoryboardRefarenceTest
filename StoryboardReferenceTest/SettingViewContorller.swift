12730a64281afe1d74730aa4aea1e663a5d2811a
sasasan03 <sako.hiro62@gmail.com>
2023-06-24 19:07:43 +0900

    StoryboardRefarenceの勉強用プロジェクト

---
+++ StoryboardReferenceTest/SettingViewContorller.swift
@@ -0,0 +1,12 @@
+//
+//  SettingViewContorller.swift
+//  StoryboardReferenceTest
+//
+//  Created by sako0602 on 2023/06/24.
+//
+
+import UIKit
+
+class SettingViewContorller {
+    
+}
---
+++ StoryboardReferenceTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StoryboardReferenceTest
+//
+//  Created by sako0602 on 2023/06/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
